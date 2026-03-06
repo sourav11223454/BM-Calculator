df7e6785266d290f95a823309c67c3c54a284583
sourav11223454 <souravvs2255@gmail.com>
2026-03-06 21:33:24 +0530

    bmi calculator

---
+++ script.js
@@ -0,0 +1,60 @@
+let height = document.getElementById('inputheight')
+let weight = document.getElementById('inputweight')
+let out = document.getElementById('output')
+let age=document.getElementById('inputage')
+
+const result = () => {
+    let h = height.value
+    let w = weight.value
+    let a=inputage.value
+
+    if(a==""){
+        alert('Enter Age')
+        return
+    }
+    let gender = document.querySelector('input[name="gender"]:checked')
+    if(gender===null){
+    alert("Please select gender")
+    return
+}
+    if (h == "" && w == "") {
+        alert('enter a valid height and weight')
+        return
+    }
+    else if (h == "" ) {
+        alert('enter a valid height')
+        return
+    }
+    else if (w == "") {
+        alert('enter a valid  weight')
+        return
+    }
+    
+    let heightInMeter = height.value / 100
+    let bmi = w / (heightInMeter * heightInMeter)
+    let category = ""
+    let color = ""
+
+    if (bmi < 18.5) {
+        category = `Underweight<i class="fa-solid fa-exclamation"></i>`
+        color = "yellow"
+    }
+    else if (bmi < 24.9) {
+        category = `Normal Weight<i class="fa-solid fa-square-check"></i>`
+        color = "green"
+    }
+    else if (bmi < 29.9) {
+        category = `Overweight<i class="fa-solid fa-triangle-exclamation"></i>`
+        color = "orange"
+    }
+    else {
+        category = `Obese<i class="fa-solid fa-exclamation"></i><i class="fa-solid fa-exclamation"></i>`
+        color = "red"
+    }
+
+    out.innerHTML = `
+<h2>Your BMI</h2>
+<h1>${bmi.toFixed(2)}</h1>
+<p style="color:${color}; font-size:22px;"><span style=color:white>Status :</span> ${category}</p>
+`
+}
